bf4b5e9c6864c55e709bb86b1144ed1a22ebbf5c
FireFighter Web <misa04@centrum.cz>
2026-06-17 12:25:54 +0200

    auto: frontend/src/app/app.routes.ts frontend/src/app/pages/article-detail/article-detail.component.html frontend/src/app/pages/article-detail/article-detail.component.scss frontend/src/app/pages/article-detail/article-detail.component.spec.ts frontend/src/app/pages/article-detail/article-detail.component.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'articles',
+    loadComponent: () => import('./pages/articles/articles.component').then((m) => m.ArticlesComponent),
+  },
+  {
+    path: 'articles/:slug',
+    loadComponent: () =>
+      import('./pages/article-detail/article-detail.component').then((m) => m.ArticleDetailComponent),
+  },
+  {
+    path: 'gallery',
+    loadComponent: () => import('./pages/gallery/gallery.component').then((m) => m.GalleryComponent),
+  },
+  {
+    path: 'gallery/:year',
+    loadComponent: () => import('./pages/gallery/gallery.component').then((m) => m.GalleryComponent),
+  },
+  {
+    path: 'gallery/:year/:event',
+    loadComponent: () => import('./pages/gallery/gallery.component').then((m) => m.GalleryComponent),
+  },
+  {
+    path: 'technics',
+    loadComponent: () => import('./pages/technics/technics.component').then((m) => m.TechnicsComponent),
+  },
+];
---
+++ frontend/src/app/pages/technics/technics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technics',
+  imports: [],
+  templateUrl: './technics.component.html',
+  styleUrl: './technics.component.scss'
+})
+export class TechnicsComponent {
+
+}
