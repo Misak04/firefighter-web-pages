2813f8f8f2e8073eab468988dcb48fa0eeb5ca65
FireFighter Web <misa04@centrum.cz>
2026-06-18 09:39:22 +0200

    auto: backend/src/media/media.module.ts

--- backend/src/media/media.module.ts
+++ backend/src/media/media.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MinioService } from './minio.service';
 import { MediaService } from './media.service';
+import { MediaController } from './media.controller';
 
 @Module({
+  controllers: [MediaController],
   providers: [MinioService, MediaService],
   exports: [MediaService, MinioService],
 })
