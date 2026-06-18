1ac924c99c636989f0a8c5a8c97e81c33d626f22
FireFighter Web <misa04@centrum.cz>
2026-06-18 12:23:22 +0200

    auto: frontend/src/app/app.routes.server.ts

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -3,6 +3,9 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   { path: 'articles', renderMode: RenderMode.Server },
   { path: 'articles/:slug', renderMode: RenderMode.Server },
+  { path: 'gallery', renderMode: RenderMode.Server },
+  { path: 'gallery/:year', renderMode: RenderMode.Server },
+  { path: 'gallery/:year/:event', renderMode: RenderMode.Server },
   { path: 'admin/**', renderMode: RenderMode.Client },
   { path: '**', renderMode: RenderMode.Prerender },
 ];
